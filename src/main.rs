bb6e9c43fe80af905d53c46cb9bc5afbaf8a3c67
zwshao <zwshao@gmail.com>
2024-09-20 22:14:26 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 mod option_op;
 
+pub mod test_trait;
 fn main() {
+    let hello = String::from("hello world");
+
 
     option_op::option_op();
+    let new_life = Life {
+        name: &hello
+    };
+
+
+    println!("{:?}", new_life);
+}
+
+fn move_life(_ : String) {
+
+}
+#[derive(Debug, Clone)]
+struct Life<'a> {
+    name: &'a String,
 }
---
+++ src/test_trait/mod.rs
@@ -0,0 +1,7 @@
+#[cfg(test)]
+pub mod test_trait_mode {
+    #[test]
+    pub fn test_trait_fn() {
+        println!("test trait");
+    }
+}
